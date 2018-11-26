5a1dade205b3990c9f77808927031615e4d890e0
“jiang.wang” <wangj9845@163.com>
2018-11-26 15:24:57 +0800

    pwa  lavas

--- .lavas/router.js
+++ .lavas/router.js
@@ -2,44 +2,44 @@ import Vue from 'vue';
 import Router from 'vue-router';
 
     
-import _9d49a922ebc8a6d77ca82c0a74289b98 from '@/pages/Appshell.vue';
+import _15432145550569d49a922ebc8a6d77ca82c0a74289b98 from '@/pages/Appshell.vue';
     
 
     
-import _f7fc7554d202a317883db2ae067316d3 from '@/pages/detail/_id.vue';
+import _1543214555056f7fc7554d202a317883db2ae067316d3 from '@/pages/detail/_id.vue';
     
 
     
-import _d2462dcf0c7beccd286c658e08187914 from '@/pages/Error.vue';
+import _1543214555056d2462dcf0c7beccd286c658e08187914 from '@/pages/Error.vue';
     
 
     
-import _67830448037326425509e44bce7632b7 from '@/pages/Index.vue';
+import _154321455505667830448037326425509e44bce7632b7 from '@/pages/Index.vue';
     
 
 
 let routes = [
     {
         "path": "/appshell",
-        "component": _9d49a922ebc8a6d77ca82c0a74289b98,
+        "component": _15432145550569d49a922ebc8a6d77ca82c0a74289b98,
         "meta": {},
         "name": "appshell"
     },
     {
         "path": "/detail/:id",
-        "component": _f7fc7554d202a317883db2ae067316d3,
+        "component": _1543214555056f7fc7554d202a317883db2ae067316d3,
         "meta": {},
         "name": "detailId"
     },
     {
         "path": "/",
-        "component": _67830448037326425509e44bce7632b7,
+        "component": _154321455505667830448037326425509e44bce7632b7,
         "meta": {},
         "name": "index"
     },
     {
         "path": "/error",
-        "component": _d2462dcf0c7beccd286c658e08187914,
+        "component": _1543214555056d2462dcf0c7beccd286c658e08187914,
         "meta": {},
         "name": "error",
         "alias": "*"
